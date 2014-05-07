12c9d28e3e49f866a06fbdac2c6f06536eaec3b9
Daniel Paulus <daniel@inuits.eu>
2014-05-07 18:32:54 +0200

    Add configfile and settings

---
+++ manifests/params.pp
@@ -0,0 +1,10 @@
+class skyline::params {
+
+  $oculus_host    = ''
+  $graphite_host  = ''
+  $carbon_port    = 2003
+  $horizon_ip     = '127.0.0.1'
+  $webapp_ip      = '127.0.0.1'
+  $webapp_port    = 1500
+
+}
