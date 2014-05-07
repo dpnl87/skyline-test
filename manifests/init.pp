d477215b86cb9ba3b45a1438611855ad79465585
Daniel Paulus <daniel@inuits.eu>
2014-05-07 17:54:07 +0200

    Moved install to init

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,59 @@
-class test{
+class skyline {
+
+  $system_packages = [
+    'python-pip',
+    'python-numpy',
+    'python-scipy',
+    'redis-server',
+    'git',
+  ]
+
+  $python_packages = [
+    'redis',
+    'hiredis',
+    'python-daemon',
+    'flask',
+    'simplejson',
+    'pandas',
+    'patsy',
+    'msgpack_python',
+    'unittest2',
+    'mock',
+  ]
+
+  $directories = [
+    '/var/log/skyline',
+    '/var/run/skyline',
+    '/var/log/redis',
+    '/var/dump',
+  ]
+
+  package { $system_packages:
+    ensure  => installed,
+    require => Apt::Source['debian'],
+  }
+
+  package { $python_packages:
+    ensure   => installed,
+    provider => pip,
+    require  => Package['python-pip'],
+  }
+
+  package { "statsmodels":
+    ensure   => installed,
+    provider => pip,
+    require  => Package['patsy'],
+  }
+
+  file { $directories:
+    ensure => directory,
+  }
+
+  vcsrepo { '/opt/skyline':
+    ensure   => present,
+    provider => 'git',
+    source   => 'https://github.com/etsy/skyline.git',
+    require  => Package['git'],
+  }
+
 }
